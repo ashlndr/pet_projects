9ffec3997527709c751c83f73578f2a2e0297458
ashlndr <sacrofize.umb@gmail.com>
2022-06-22 20:59:32 +0700

    Added test_main_page

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="Choose browser language")
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser to run")
